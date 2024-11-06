63c8ff4af04525b493ac90d7923f4f0cb351c13e
taariqjp <taariqjp@gmail.com>
2024-11-06 09:45:01 -0300

    Update 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # What is the hash of block 654,321?
 bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C getblockhash 624351
 # Out -> 00000000000000000000ceb1595db5c8ea7080f5b0fdade1b6ce65074694d8ea
