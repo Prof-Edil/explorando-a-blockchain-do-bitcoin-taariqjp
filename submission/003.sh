9534280502fdb0038151c776c5f33a947226aff5
taariqjp <taariqjp@gmail.com>
2024-11-08 11:29:57 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,4 +1,4 @@
 # How many new outputs were created by block 123,456?
 #!/bin/bash
-bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C getblockstats 123456
+bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C getblockstats 123456 | jq '.outs'
 #outs -> 24
