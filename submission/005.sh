64c65015448bb4dabee0f469d3e6ad21785b36b0
taariqjp <taariqjp@gmail.com>
2025-02-02 21:31:28 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -4,14 +4,14 @@
 
 tx=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C getrawtransaction "37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517" 2)
 
-xpub1=$(echo "$tx1" | jq  -r '.vin[0].txinwitness[1]')
+xpub1=$(echo "$tx" | jq  -r '.vin[0].txinwitness[1]')
 
-xpub2=$(echo "$tx1" | jq  -r '.vin[1].txinwitness[1]')
+xpub2=$(echo "$tx" | jq  -r '.vin[1].txinwitness[1]')
 
-xpub3=$(echo "$tx1" | jq  -r '.vin[2].txinwitness[1]')
+xpub3=$(echo "$tx" | jq  -r '.vin[2].txinwitness[1]')
 
-xpub4=$(echo "$tx1" | jq  -r '.vin[3].txinwitness[1]')
+xpub4=$(echo "$tx" | jq  -r '.vin[3].txinwitness[1]')
 
-multisig=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C createmultisig 1 "[\"$pub1\",\"$pub2\",\"$pub3\",\"$pub4\"]")
+multisig=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_262 -rpcpassword=l6i5arFr3h8C createmultisig 1 "[\"$xpub1\",\"$xpub2\",\"$xpub3\",\"$xpub4\"]")
 
 echo $multisig | jq -r '.address'
